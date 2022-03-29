9fccaae111c86512911e5ce70db2b42d5e903352
dmeleskausas <meleskausas@inbox.ru>
2022-03-30 00:00:53 +0300

    Player added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-const width 		= 900;
-const height 		= 600;
-const groundHeight 	= 150;
-const wallWidth 	= 50;
-const wallOffset 	= wallWidth * 2;
-const gapHeight 	= 75;
-const wallColor 	= 'rgb(200, 0, 0)';
-const wallsCount 	= (width / (wallWidth + wallOffset)) + 1;
+const width 		  = 900;
+const height 		  = 600;
+const groundHeight    = 150;
+const wallWidth 	  = 50;
+const wallOffset 	  = wallWidth * 2;
+const gapHeight 	  = 75;
+const wallColor 	  = 'rgb(200, 0, 0)';
+const wallsCount 	  = (width / (wallWidth + wallOffset)) + 1;
+const playerSize      = 30;
+const playerDownSpeed = playerSize * 3;
+const playerUpSpeed   = playerSize * 500;
 
 const canvas = document.getElementById('screen');
 const ctx 	 = canvas.getContext('2d');
@@ -24,19 +27,22 @@ const randomInt = (min, max) => {
 };
 
 const randomGapPositionY = () => {
-	return randomInt(gapHeight, height - (gapHeight + gapHeight * 2 + groundHeight));
+	// return randomInt(gapHeight, height - (gapHeight + gapHeight * 2 + groundHeight));
+	return randomInt(gapHeight, height - (gapHeight + gapHeight + groundHeight));
 };
 
 const walls = [];
 
 for (let i = 0; i < wallsCount; ++i) {
-	walls.push({
-		wallPositionX: (wallWidth + wallOffset) * i,
-		gapPositionY:  randomGapPositionY(),
-	});
+	// walls.push({
+	// 	wallPositionX: width + (wallWidth + wallOffset) * i,
+	// 	gapPositionY:  randomGapPositionY(),
+	// });
 }
 
-const fps  = 60.0;
+let playerPositionY = (height - groundHeight) / 2;
+
+const fps  = 144.0;
 const loop = setInterval(() => {
 	// CLEAR CANVAS
 	ctx.clearRect(0, 0, 1100, height);
@@ -52,11 +58,38 @@ const loop = setInterval(() => {
 
 	// MOVES WALLS
 	for (let wall of walls) {
-		wall.wallPositionX -= wallWidth / (1000.0 / fps);
+		wall.wallPositionX -= wallWidth / fps;
 
 		if (wall.wallPositionX + wallWidth <= 0) {
 			wall.wallPositionX += width + wallWidth + wallOffset;
 			wall.gapPositionY   = randomGapPositionY();
 		}
 	}
+
+	// ctx.fillStyle = 'rgb(0, 200, 200)';
+	// ctx.fillRect((width / 2) - (playerSize), playerPositionY, playerSize, (height - groundHeight) / 2);
+
+	// DRAWS PLAYER
+	ctx.fillStyle = 'rgb(0, 0, 200)';
+	ctx.fillRect((width / 2) - (playerSize / 2), playerPositionY - (playerSize / 2), playerSize, playerSize);
+	
+	// MOVES PLAYER
+	playerPositionY += playerDownSpeed / fps;
+
+	document.onkeydown = function upup(up)
+{
+    if (up.keyCode == '32')
+    {
+    playerPositionY -= playerUpSpeed / fps;
+    }
+};
+
+	if (playerPositionY + playerSize / 2 >= height - groundHeight) {
+		playerPositionY = height - groundHeight - playerSize / 2;
+	}
+    if (playerPositionY - playerSize / 2 <= 0) {
+		playerPositionY = playerSize / 2;
+	}
+
 }, 1000.0 / fps);
+
